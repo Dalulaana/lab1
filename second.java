1373936d87f835e1facfe47ee518f543bc43aeb1
Dalulaana <125021902+Dalulaana@users.noreply.github.com>
2023-07-22 23:06:59 +0600

    Add files via upload

---
+++ second.java
@@ -0,0 +1,23 @@
+public class second
+{
+    public static double average(int n, int[] arr) 
+    {
+        if (arr==null || arr.length!=n) 
+        {
+            throw new IllegalArgumentException("are you ok?");
+        }
+        double sum=0;
+        for (int i=0; i<n; i++) 
+        {
+            sum+=arr[i];
+        }
+        return sum/n;
+    }
+    public static void main(String[] args) 
+    {
+        int[] numbers = {3, 2, 4, 1};
+
+        double average = average(numbers.length, numbers);
+        System.out.println("average: " + average);
+    }
+}
